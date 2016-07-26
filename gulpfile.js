6b9a5d37b1d7fd0e954b1d041726a76cf7322cf3
Niall Duncan <niall@pixelunion.net>
2016-07-26 13:56:40 -0400

    Add pre-commit script for gulp. Remove some dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,12 @@
 var gulp = require('gulp');
 var babel = require('gulp-babel');
+var uglify = require('gulp-uglify');
 
 gulp.task('bundle', function() {
   gulp.src('./src/js/loading.js')
     .pipe(babel({
       presets: ['es2015']
     }))
+    .pipe(uglify())
     .pipe(gulp.dest('./dist/js/'));
 });
