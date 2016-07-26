4ff3f7df84ae9036a617721af4655d7df17c74ba
Niall Duncan <niall@pixelunion.net>
2016-07-26 11:49:25 -0400

    fix up a few Gulp issues

--- dist/js/loading.js
+++ dist/js/loading.js
@@ -1,48 +1,41 @@
-import $ from 'jquery';
-import 'jquery-trend';
+'use strict';
 
-var classCallCheck = function (instance, Constructor) {
-  if (!(instance instanceof Constructor)) {
-    throw new TypeError("Cannot call a class as a function");
-  }
-};
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
 
-var createClass = function () {
-  function defineProperties(target, props) {
-    for (var i = 0; i < props.length; i++) {
-      var descriptor = props[i];
-      descriptor.enumerable = descriptor.enumerable || false;
-      descriptor.configurable = true;
-      if ("value" in descriptor) descriptor.writable = true;
-      Object.defineProperty(target, descriptor.key, descriptor);
-    }
-  }
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
-  return function (Constructor, protoProps, staticProps) {
-    if (protoProps) defineProperties(Constructor.prototype, protoProps);
-    if (staticProps) defineProperties(Constructor, staticProps);
-    return Constructor;
-  };
-}();
+var _jquery = require('jquery');
+
+var _jquery2 = _interopRequireDefault(_jquery);
+
+var _jqueryTrend = require('jquery-trend');
+
+var _jqueryTrend2 = _interopRequireDefault(_jqueryTrend);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 var LoadingUtils = function () {
   function LoadingUtils(options, scrollLock, el) {
-    classCallCheck(this, LoadingUtils);
+    _classCallCheck(this, LoadingUtils);
 
     this.scrollLock = scrollLock;
-    this.$body = $(document.body);
-    this.$el = el ? $(el) : this.$body;
+    this.$body = (0, _jquery2.default)(document.body);
+    this.$el = el ? (0, _jquery2.default)(el) : this.$body;
 
-    this.options = $.extend({
+    this.options = _jquery2.default.extend({
       loadingMarkup: '<div class="loading"><span class="loading-spinner"></span></div>',
       visibleClass: 'visible',
       scrollLockClass: 'scroll-locked'
     }, options);
 
-    this.$loading = $(this.options.loadingMarkup);
+    this.$loading = (0, _jquery2.default)(this.options.loadingMarkup);
   }
 
-  createClass(LoadingUtils, [{
+  _createClass(LoadingUtils, [{
     key: 'show',
     value: function show() {
       var _this = this;
@@ -77,7 +70,8 @@ var LoadingUtils = function () {
       });
     }
   }]);
+
   return LoadingUtils;
 }();
 
-export default LoadingUtils;
+exports.default = LoadingUtils;
--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,12 @@
 var gulp = require('gulp');
-var rollup = require('gulp-rollup')
-var babel = require('rollup-plugin-babel');
-var commonjs = require('rollup-plugin-commonjs');
+var babel = require('gulp-babel');
 
 gulp.task('default', ['bundle']);
 
 gulp.task('bundle', function() {
-  gulp.src('./src/js/**.js')
-    .pipe(rollup({
-      entry: './src/js/loading.js',
-      plugins: [
-        babel({
-          babelrc: false,
-          exclude: 'node_modules/**/*',
-          presets: ['es2015-rollup']
-        }),
-        commonjs()
-      ]
+  gulp.src('./src/js/loading.js')
+    .pipe(babel({
+      presets: ['es2015']
     }))
     .pipe(gulp.dest('./dist/js/'));
 });
