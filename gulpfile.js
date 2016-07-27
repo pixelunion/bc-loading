93a92781215baf5213dd00d82fc6a23e27d118e6
Niall Duncan <niall@pixelunion.net>
2016-07-27 08:33:36 -0400

    Remove uglify

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,10 @@
 var gulp = require('gulp');
 var babel = require('gulp-babel');
-var uglify = require('gulp-uglify');
 
 gulp.task('bundle', function() {
   gulp.src('./src/loading.js')
     .pipe(babel({
       presets: ['es2015']
     }))
-    .pipe(uglify())
     .pipe(gulp.dest('./dist/'));
 });
